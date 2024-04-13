740b5f13fc0756128c2cbb4cdfb615d37355ad67
Benjamin Bachmann <benjamin.bachmann@zuehlke.com>
2024-04-13 12:30:13 +0200

    feat: add some sample world objects

--- ld55/src/world.rs
+++ ld55/src/world.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_rts_camera::Ground;
 pub struct Plugin;
 
 impl bevy::prelude::Plugin for Plugin {
@@ -8,18 +9,70 @@ impl bevy::prelude::Plugin for Plugin {
     }
 }
 
-#[derive(Component)]
-pub struct Ground;
-
 #[allow(clippy::needless_pass_by_value)]
-fn setup(mut meshes: ResMut<Assets<Mesh>>, mut commands: Commands) {
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // Ground
     commands.spawn((
         PbrBundle {
-            mesh: meshes.add(Plane3d::default().mesh().size(10.0, 10.0)),
+            mesh: meshes.add(Plane3d::default().mesh().size(80.0, 80.0)),
+            material: materials.add(Color::rgb(0.3, 0.5, 0.3)),
             ..default()
         },
         Ground,
     ));
+
+    // Some "terrain"
+    let terrain_material = materials.add(Color::rgb(0.8, 0.7, 0.6));
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(Cuboid::new(15.0, 1.0, 5.0)),
+            material: terrain_material.clone(),
+            transform: Transform::from_xyz(15.0, 0.5, -5.0),
+            ..default()
+        },
+        Ground,
+    ));
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(Cuboid::new(10.0, 5.0, 15.0)),
+            material: terrain_material.clone(),
+            transform: Transform::from_xyz(-15.0, 2.5, 0.0),
+            ..default()
+        },
+        Ground,
+    ));
+
+    // Some generic units that are not part of the 'Ground' (ignored for height calculation)
+    for x in -5..5 {
+        for z in -5..5 {
+            commands.spawn(PbrBundle {
+                mesh: meshes.add(Capsule3d::new(0.25, 1.25)),
+                material: terrain_material.clone(),
+                transform: Transform::from_xyz(x as f32 * 0.7, 0.75, z as f32 * 0.7),
+                ..default()
+            });
+        }
+    }
+
+    // Light
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            illuminance: 1000.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_rotation(Quat::from_euler(
+            EulerRot::YXZ,
+            150.0f32.to_radians(),
+            -40.0f32.to_radians(),
+            0.0,
+        )),
+        ..default()
+    });
 }
 
 fn update() {}
